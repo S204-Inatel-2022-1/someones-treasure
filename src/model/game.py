065920f9f614df0d16efd89dae0c64d4489cb1be
bio353 <fabiomcn2001@gmail.com>
2022-03-19 10:38:29 -0300

    Added player movement

--- src/model/game.py
+++ src/model/game.py
@@ -6,8 +6,11 @@ class Game():
     def __init__(self):
         self.window = pygame.display.set_mode(WINDOW_SIZE)
         pygame.display.set_caption("Someone's Treasure")
-        file_name = 'assets/img/sprites/game_icon.png'
+        file_name = 'assets/img/game_icon.png'
         pygame.display.set_icon(pygame.image.load(file_name).convert_alpha())
-        # Hm...
         self.clock = pygame.time.Clock()
         self.running = True
+        '''
+        self.current_lvl = 1
+        self.lvls = []
+        '''
--- src/model/player.py
+++ src/model/player.py
@@ -1,5 +1,5 @@
 import pygame
-from src.helper.settings import TILE_SIZE, PLAYER_SPEED
+from src.helper.settings import TILE_SIZE
 
 
 class Player(pygame.sprite.Sprite):
@@ -10,29 +10,25 @@ class Player(pygame.sprite.Sprite):
         self.dirvec = pygame.math.Vector2(0, 0)
         self.last_pos = self.pos
         self.next_pos = self.pos
-
         self.current_frame = 0
         self.last_update = pygame.time.get_ticks()
         self.between_tiles = False
-
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(topleft=(self.pos.x, self.pos.y))
+        self.player_speed = 3 * TILE_SIZE
 
     def update(self, dt, walls):
         self.get_keys()
         self.rect = self.image.get_rect(topleft=(self.pos.x, self.pos.y))
-
         if self.pos != self.next_pos:
-
             delta = self.next_pos - self.pos
-            if delta.length() > (self.dirvec * PLAYER_SPEED * dt).length():
-                self.pos += self.dirvec * PLAYER_SPEED * dt
+            if delta.length() > (self.dirvec * self.player_speed * dt).length():
+                self.pos += self.dirvec * self.player_speed * dt
             else:
                 self.pos = self.next_pos
                 self.dirvec = pygame.math.Vector2(0, 0)
                 self.between_tiles = False
-
         self.rect.topleft = self.pos
         if pygame.sprite.spritecollide(self, walls, False):
             self.pos = self.last_pos
@@ -44,10 +40,8 @@ class Player(pygame.sprite.Sprite):
     def get_keys(self):
         now = pygame.time.get_ticks()
         keys = pygame.key.get_pressed()
-
         if now - self.last_update > self.walk_buffer:
             self.last_update = now
-
             new_dir_vec = pygame.math.Vector2(0, 0)
             if self.dirvec.y == 0:
                 if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -59,7 +53,6 @@ class Player(pygame.sprite.Sprite):
                     new_dir_vec = pygame.math.Vector2(0, -1)
                 elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
                     new_dir_vec = pygame.math.Vector2(0, 1)
-
             if new_dir_vec != pygame.math.Vector2(0, 0):
                 self.dirvec = new_dir_vec
                 self.between_tiles = True
