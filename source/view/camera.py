df413639be5a2049b6c6c0307b09e58030406bae
bio353 <fabiomcn2001@gmail.com>
2022-04-25 22:33:13 -0300

    Updated camera

--- source/view/camera.py
+++ source/view/camera.py
@@ -2,21 +2,20 @@ import pygame as pg
 
 
 class Camera(pg.sprite.Group):
-    def __init__(self, index: int):
+    def __init__(self):
         super().__init__()
         self.display_surface = pg.display.get_surface()
         self.half_width = self.display_surface.get_width() // 2
         self.half_height = self.display_surface.get_height() // 2
         self.offset = pg.math.Vector2()
-        img_path = f"assets/images/map/{index}.png"
-        self.floor_surface = pg.image.load(img_path).convert()
-        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+        self.ground_surf = pg.image.load("assets/images/map.png").convert()
+        self.ground_rect = self.ground_surf.get_rect(topleft=(0, 0))
 
-    def custom_draw(self, player: pg.sprite.Sprite):
+    def custom_draw(self, player):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
-        self.floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surface, self.floor_offset_pos)
+        self.ground_offset_pos = self.ground_rect.topleft - self.offset
+        self.display_surface.blit(self.ground_surf, self.ground_offset_pos)
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_position = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_position)
