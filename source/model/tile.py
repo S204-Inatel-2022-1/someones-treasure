f553b745e32aa7941fd439c936b0d734bd3b1154
bio353 <fabiomcn2001@gmail.com>
2022-05-01 15:00:36 -0300

    Updated tile

--- source/model/tile.py
+++ source/model/tile.py
@@ -4,9 +4,9 @@ from source.utils.settings import TILE_SIZE
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, groups, pos, surface=pg.surface.Surface((TILE_SIZE, TILE_SIZE)), has_collision=False):
+    def __init__(self, groups, pos, surface=pg.surface.Surface((TILE_SIZE, TILE_SIZE))):
         super().__init__(groups)
         pos = np.multiply(pos, TILE_SIZE)
         self.image = surface
         self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(- TILE_SIZE // 16, - TILE_SIZE // 4)
+        self.hitbox = self.rect.inflate(- TILE_SIZE // 4, - TILE_SIZE // 4)
