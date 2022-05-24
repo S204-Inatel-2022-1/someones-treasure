fb5723fe9ea46964425f6352972ad493c325712f
bio353 <fabiomcn2001@gmail.com>
2022-05-24 19:34:24 -0300

    Updated main

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-from source.controller.game import Game
-from source.utils.cli_helper import cls
+import pygame as pg
+import sys
+from scripts.constants import *
+from scripts.level import Level
+from scripts.utils import clear_screen
 
 
-if __name__ == '__main__':
-    cls()
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RESOLUTION)
+        pg.display.set_caption("Someone's Treasure")
+        icon = pg.image.load("images/icons/game-icon.png").convert_alpha()
+        pg.display.set_icon(icon)
+        self.clock = pg.time.Clock()
+        self.level = Level()
+
+    def play(self):
+        while True:
+            self.clock.tick(FPS)
+            self.screen.fill("black")
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.close()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        self.close()
+                    if event.key == pg.K_RETURN:
+                        self.level.pause()
+            self.level.run()
+            pg.display.update()
+
+    def close(self):
+        print("GAME OVER...\n")
+        pg.quit()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    clear_screen()
     game = Game()
-    game.run()
+    game.play()
