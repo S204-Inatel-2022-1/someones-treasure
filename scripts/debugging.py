1490310d7cb22bbb678cd937f3fae204f20cfc0c
bio353 <fabiomcn2001@gmail.com>
2022-05-25 00:00:46 -0300

    This is not being used anymore

---
+++ scripts/debugging.py
@@ -0,0 +1,20 @@
+import pygame as pg
+
+
+def display_info(info):
+    display_surface = pg.display.get_surface()
+    font = pg.font.SysFont("Arial", 30)
+    debug_surf = font.render(str(info), True, "green")
+    debug_rect = debug_surf.get_rect(topleft=(10, 10))
+    pg.draw.rect(display_surface, "green", debug_rect, 1)
+    display_surface.blit(debug_surf, debug_rect)
+
+
+def show_grid():
+    display_surface = pg.display.get_surface()
+    for i in range(0, display_surface.get_width(), 64):
+        pg.draw.line(display_surface, "green", (i, 0),
+                     (i, display_surface.get_height()))
+    for i in range(0, display_surface.get_height(), 64):
+        pg.draw.line(display_surface, "green", (0, i),
+                     (display_surface.get_width(), i))
